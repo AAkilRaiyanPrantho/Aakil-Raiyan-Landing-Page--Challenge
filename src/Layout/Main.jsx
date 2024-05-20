271071bac68eb7f69bd1b2f959c2c2116601d781
MDAakilRaiyan <aakil15-11060@diu.edu.bd>
2024-05-20 23:28:24 +0600

    Navbar and Footer initially created

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,5 +1,6 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../Components/Navbar/Navbar";
+import Footer from "../Components/Footer/Footer";
 
 
 const Main = () => {
@@ -7,6 +8,7 @@ const Main = () => {
         <div>
             <div className="max-w-screen-xl mx-auto"><Navbar></Navbar></div>
             <Outlet></Outlet>
+            <div className="max-w-screen-xl mx-auto"><Footer></Footer></div>
         </div>
     );
 };
